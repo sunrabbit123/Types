6d7030f0ed22be7add6540173f991a39218ca058
sunrabbit123 <qudwls185@naver.com>
2022-01-06 19:36:26 +0900

    update : class of types

--- normal.ts
+++ normal.ts
@@ -1,6 +1,7 @@
 const str1: string = "it is example string";
 const str2: "It is Literal string types" = "It is Literal string types";
 const str3: "Both" | "A" | "or" | "B" = "Both";
+String();
 
 const num1: number = 1;
 const num2: 1 | 2 = 2; // It is Literal number types;
@@ -9,11 +10,15 @@ const hex: number = 0xf00d;
 const binary: number = 0b1010;
 const octal: number = 0o744;
 const big: bigint = 100n;
+Number();
+BigInt();
 
 const isDone: boolean = false;
+Boolean();
 
 const numberList: number[] = [1, 2, 3];
 const numberList2: Array<number> = [1, 2, 3];
+Array<number>();
 
 const stringNumberTuple: [string, number] = ["hello", 10];
 // const stringNumberTuple : [string, number] = ["hello", 10];
