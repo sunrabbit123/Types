7a33fe5a7a1e68d1e4e63f32605cec37ffa89429
sunrabbit123 <qudwls185@naver.com>
2022-01-09 20:11:57 +0900

    Add : util types

--- util.ts
+++ util.ts
@@ -39,3 +39,47 @@ type PostDescription = Omit<Post, "title">; // without title
 const examPost3: PostDescription = {
   description: "asdf",
 };
+
+// 2번째 인자값을 제외함
+type T0 = Exclude<"a" | "B" | "c", "B">; // "a" | "c"
+type T1 = Exclude<string | number | (() => void), Function>; // string | number
+
+// 둘의 교집합
+type T2 = Extract<"a" | "b" | "c", "a" | "f">; // "a"
+type T3 = Extract<string | number | (() => void), Function>; // () => void
+
+type NonNullType = NonNullable<string | number | undefined>;
+
+// 함수 타입의 매개변수 타입들로 튜플타입을 구성함
+declare function f1(arg: { a: number; b: string }): void;
+type T4 = Parameters<typeof f1>;
+
+interface constructorInterface {
+  new (message: string, code: number): undefined;
+}
+
+type T5 = ConstructorParameters<constructorInterface>; // [message : string, code : number]
+type T6 = ConstructorParameters<FunctionConstructor>; // string[]
+type T7 = ConstructorParameters<ErrorConstructor>;
+type T8 = ConstructorParameters<RegExpConstructor>;
+const some1: T5 = ["asdf", 123];
+
+// 반환값에 대한 타입
+type T9 = ReturnType<typeof f1>;
+
+//
+class C {
+  x = 0;
+  y = 0;
+}
+type T10 = InstanceType<typeof C>;
+type T11 = C;
+const O1: T10 = new C();
+
+interface Props {
+  a?: number;
+  b?: string;
+}
+
+const O2: Props = { a: 5 };
+const O3: Required<Props> = { a: 5, b: "asdf" }; //a, ,b is required
