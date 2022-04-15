d9842a014bc9687ecb5c5ecfb26924af9688beec
sunrabbit123 <qudwls185@naver.com>
2022-04-15 15:59:06 +0900

    update: extends 삼항연산자

--- extends.ts
+++ extends.ts
@@ -4,3 +4,6 @@ type CopyRecord<K extends string | number | symbol, T> = { [P in K]: T };
 type Test1<K extends number> = { [P in K]: string };
 
 const example1: Test1<1 | 2 | 3> = { 1: "test1", 2: "test2", 3: "test3" };
+
+type Test2<T1, T2> = T1 extends T2 ? T1 : T2;
+const example2: Test2<string, number> = 123;
